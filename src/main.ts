c7a8e23faa7acea1c5d2e91d3f68f45a34a4f2bb
Eduardo Toledo <eduardo.toledo@keyland.es>
2021-10-06 13:50:40 +0200

    prueba

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { ExecFileSyncOptionsWithBufferEncoding } from 'child_process';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -10,3 +11,42 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+////////////////////
+class Persona {
+
+  constructor(
+    public Nombre: string,
+    public Apellidos: string = ""
+  ) {
+
+  }
+
+}
+
+class Heroe extends Persona {
+
+  constructor(
+    public Nombre: string,
+    public Apellidos: string,
+    public Nick: string,
+    public Poder: string
+  ) {
+    super(Nombre, Apellidos);
+
+  }
+
+  Info(): string {
+    let valor = "";
+    valor = this.Nick + " es " + this.Nombre + " " + this.Apellidos;
+    return valor;
+  }
+
+}
+
+
+
+const personaje: Heroe = new Heroe("Peter", "Parker", "Spiderman", "Aracnido");
+
+console.log(personaje);
+console.log(personaje.Info());
